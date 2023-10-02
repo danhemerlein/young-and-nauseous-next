0c04c21326a5a3aabd7a398e6efdac8e4e6a2270
Dan Hemerlein <dan@halfhelix.com>
2023-10-02 14:22:21 -0400

    move homepage to its own page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
-import products from '../products.json'
-import ProductCard from '@/components/ProductCard'
+import HomePage from '@/pages/index'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 grid-cols-3 lg:text-left">
-        {products.map((product) => {
-          return <ProductCard key={product.id} product={product} />
-        })}
-      </ul>
+      <HomePage />
     </main>
   )
 }
