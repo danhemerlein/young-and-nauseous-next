53f8a151308172461d4ec24ecac9b1569e4ccb55
Dan Hemerlein <danhemerlein@gmail.com>
2024-02-01 22:09:35 -0500

    keys

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 'use client'
 
-import Layout from '@/components/layout'
+import Layout from '@/components/Layout'
 import { CartContext, UseCartState } from '@/hooks/UseCart'
 import { AuthContext, UseAuthState } from '@/hooks/UseAuth'
 import { ModalContext, UseModalState } from '@/hooks/UseModal'
